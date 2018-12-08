fe053de10694fc1867e33cf0871121c759027a6f
andreabruera <andrea.bruera@gmail.com>
2018-12-08 12:50:16 +0100

    Removed the 100 books.

--- scripts/get_books/download_books.py
+++ scripts/get_books/download_books.py
@@ -8,6 +8,6 @@ for l in filename:
     b=requests.get('http://www.gutenberg.org/cache/epub/{}/pg{}.txt'.format(line[0], line[0])).text
     name=line[1].replace(' ','_')
     name2=''.join([l for i,l in enumerate(name) if l.isalpha() and i<40 or l=='_' and i<40])
-    os.makedirs('100_books/{}'.format(name2), exist_ok=True)
-    out=open('100_books/{}/{}.txt'.format(name2, line[0]),'w')
+    os.makedirs('../../100_books/{}'.format(name2), exist_ok=True)
+    out=open('../../100_books/{}/{}.txt'.format(name2, line[0]),'w')
     out.write(b)
